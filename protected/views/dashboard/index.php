7e536a2606cdc200f88ed3bc92b2251a1edec564
ndloc1905 <ndloc1905@gmail.com>
2013-08-27 00:50:51 +0700

    add dashboard controller and content

---
+++ protected/views/dashboard/index.php
@@ -0,0 +1,84 @@
+<h1>Dashboard</h1>    
+    <div class="row">
+      <div class="col-lg-2">
+        <div class="sidebar">
+          <a href="#stats" class="btn btn-link"><span class="glyphicon glyphicon-comment"></span>Messages</a>
+          <a href="#stats" class="btn btn-link"><span class="glyphicon glyphicon-stats"></span>Statistic</a>
+          <a href="#profile" class="btn btn-link on-air"><span class="glyphicon glyphicon-user"></span>Profile</a>
+          <a href="#stats" class="btn btn-link"><span class="glyphicon glyphicon-list"></span>Services</a>
+          <hr>
+          <a href="#logout" class="btn btn-link"><span class="glyphicon glyphicon-log-out"></span>Logout</a>
+        </div>        
+      </div>
+      <div class="col-lg-10">
+        <div class="workplace">
+          <h3>Edit Your Profile</h3>
+          <div class="row">
+            <div class="col-lg-3">              
+              <p><img src="http://placehold.it/200x200"></p>
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-default">Change Profile Picture</button>              
+                <button type="button" class="btn btn-sm btn-danger">Delete</button> 
+              </div>
+            </div>
+            <div class="col-lg-9">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Basic Information</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="col-lg-6 row">
+                    <div class="form-group">
+                      <h5>Email address: <span class="glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="top" title="" data-original-title="Allow use multiple email for an account."></span></h5>
+                      <p><b>hk_phuoc@live.com</b></p>
+                      <p class="input-group">
+                        <span class="input-group-addon"><a href="#"><span class="glyphicon glyphicon-plus-sign"></span></a></span>
+                        <input type="text" class="form-control" id="exampleInput" placeholder="Enter Email Address">                      
+                      </p>                    
+                      <a href="#">Add another email</a>
+                    </div>
+                    <div class="form-group">
+                      <h5>Full Name</h5>
+                      <p class="input-group">                        
+                        <input type="text" class="form-control" id="exampleInput" placeholder="Input Your Name" value="Phuoc Huynh">
+                        <span class="input-group-addon color-success"><span class="glyphicon glyphicon-ok"></span></span>                        
+                      </p>                      
+                    </div>
+                    <div class="form-group">
+                      <h5>Title</h5>
+                      <p class="input-group">                        
+                        <input type="text" class="form-control" id="exampleInput" placeholder="Name Your Title" value="">
+                        <span class="input-group-addon color-fail"><span class="glyphicon glyphicon-remove"></span></span>                        
+                      </p>                      
+                    </div>
+                    <div class="form-group">
+                      <h5>Bio</h5>
+                      <textarea class="form-control" placeholder="Give some words to talk about yourself" rows="4"></textarea>                   
+                    </div>
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-info">Update</button>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Change Password</h3>
+                </div>
+                <div class="panel-body">  
+                  <div class="col-lg-6 row">
+                    <p><h5>Old Password</h5><input type="password" class="form-control" id="exampleInputPassword" placeholder="Input Old Password"></p>
+                    <p><h5>New Password</h5><input type="password" class="form-control" id="exampleInputPassword" placeholder="Input New Password"></p>
+                    <p><h5>Confirm Password</h5><input type="password" class="form-control" id="exampleInputPassword" placeholder="Confirm Password Again"></p>
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-info">Update</button>
+                    </div> 
+                  </div>                                   
+                </div>
+              </div>
+            </div>
+          </div>
+                   
+        </div>        
+      </div>
+    </div>
