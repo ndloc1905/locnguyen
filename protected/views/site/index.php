8169d2f25c4b7dc1d176f4ce75aaeea184641831
ndloc1905 <ndloc1905@gmail.com>
2013-08-27 00:37:15 +0700

    add index content

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,81 @@
-<?php
-/* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name;
-?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div class="row">
+      <div class="col-lg-8">
+        <h1>Welcome to GreenSocial - Professional Services</h1>
+        <p>GreenSocial for Professional Services is a prototype of Social Network Platform built on top of <em>Yii framework and Bootstrap framework</em>. It is allow user register for an account, update their profile and ready to use for a lots of <b>additional features</b> as an add-ons later.</p>
+        <p>
+        <div class="row">
+          <div class="col-lg-6">
+            <h3><span class="glyphicon glyphicon-user"></span> Update Your Profile</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet porttitor nulla. Vestibulum viverra sodales venenatis. Ut justo tortor, dapibus eu porta quis, pulvinar sed leo.</p>
+          </div>
+          <div class="col-lg-6">
+            <h3><span class="glyphicon glyphicon-heart"></span> Make People Love</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet porttitor nulla. Vestibulum viverra sodales venenatis. Ut justo tortor, dapibus eu porta quis, pulvinar sed leo.</p>
+          </div>
+          <div class="col-lg-6">
+            <h3><span class="glyphicon glyphicon-star"></span> Become Popular</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet porttitor nulla. Vestibulum viverra sodales venenatis. Ut justo tortor, dapibus eu porta quis, pulvinar sed leo.</p>
+          </div>
+          <div class="col-lg-6">
+            <h3><span class="glyphicon glyphicon-usd"></span> Get Your Revenue</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet porttitor nulla. Vestibulum viverra sodales venenatis. Ut justo tortor, dapibus eu porta quis, pulvinar sed leo.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div id="register" class="panel panel-success">
+          <div class="panel-heading">
+            New to GreenSocial? <b>Sign Up!</b>
+          </div>
+          <div class="panel-body">            
+            <div class="form-group">
+              <label for="InputName">Your Name</label>
+              <input type="text" class="form-control" id="InputName" placeholder="Enter your name">
+            </div>          
+            <div class="form-group">
+              <label for="InputEmail">Email Address</label>
+              <input type="text" class="form-control" id="InputEmail" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="InputPassword">Password</label>
+              <input type="password" class="form-control" id="InputPassword" placeholder="Password">
+            </div>         
+            <div class="btn-group">
+              <button type="button" class="btn btn-success">Join Now</button>              
+            </div>
+          </div>
+        </div>
+        <div id="hallfame">
+          <h4>Top Popular Users</h4>
+          <div class="table-responsive">
+            <table class="table">
+              <tr>
+                <th>#</th>
+                <th>User</th>
+                <th>Popularity<th>                
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Phuoc H.</td>
+                <td>1523<td>                
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Joker V.</td>
+                <td>1500<td>                
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Cristiano R.</td>
+                <td>1489<td>                
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Robin V. P.</td>
+                <td>1420<td>                
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
